3a3d507d7aafcdb35013384c59c4831b7de637a6
krypton-0x00 <shakirgulzar0x00@gmail.com>
2024-08-26 15:41:16 +0530

    Recursion/countingNumbersReverse

--- Recursion/countingNumbersReverse.cpp
+++ Recursion/countingNumbersReverse.cpp
@@ -5,13 +5,16 @@
 using namespace std;
 
 void countingNumbers(int n){
-    if(n==0) return ;
-    LOG(n);
+    if(n<=0) return ;
     countingNumbers(n-1);
+    LOG(n);
     
 }
 
 
 int main(){
+
     countingNumbers(5);
+
+
 }
