47f992c5ced1cff6830baa639b8d4a00de366eb3
krypton-0x00 <shakirgulzar0x00@gmail.com>
2024-08-27 14:37:39 +0530

    Recursion/spellNumberProblem

---
+++ Recursion/spellNumber.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define LOG(message) cout<<message<<endl;
+
+using namespace std;
+
+
+
+void spell(int n ,string str[]){
+    if(n == 0) return;
+
+    
+   spell(n/10,str);
+   LOG(str[n%10]);
+
+}
+
+int main(){
+    string nums[10]={"zero","one","two","three","four","five","six","seven","eight","nine"};
+   spell(3,nums);
+} 
